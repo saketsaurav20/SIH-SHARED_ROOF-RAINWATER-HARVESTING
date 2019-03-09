33c023c54c47c906f61805986508cba1f56fdf15
SS <saket.saurav20@gmail.com>
2019-03-09 12:10:43 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# SIH-SHARED_ROOF-RAINWATER-HARVESTING
+# SIH-RainWater-Harvesting
+SIH submission for GoldMan Sachs' problem statement at NIT Calicut. 
+- At this time, the entire project is ready to be deployed and runs on a Flask middle-tier. All values are real-time satellite images obtained from Google Maps API for the rooftop detection and estimation.   
+- The machine learning aspect of our project was used to estimate the dimensions of the roof (`OpenCV` and `Computer Vision Models`) along with rainfall estimation using a rolling forecasting model. 
+  
+### Planned components:
+1. Rooftop Detection (`Watershed Algorithm` + `Canny Edge Detection`)
+2. Depth Estimation + Volume Calculation (Of the water collected)
+3. Rainfall Prediction
+4. Calculator (Cost of installation + Break-Even analysis)
+5. Tax Benefits
+6. Non-Drinking Water Usage Prediction
+7. Basic Shared Rainwater Harvesting System (Phase 1)
+8. Water Credit System Plan (Phase 2)
+---
+### Problem Statement
+Currently, for roof top rain water harvesting, people install water storage tanks individually per building/apartment which results in high cost for individuals/groups. No mechanism/application is available to find out where such installations are beneficial, which installations can share storage tanks and what would be the required capacity of these shared tanks. Given map and housing data, optimize the location of centralized tanks for rain-water harvesting.The following data should be sufficient to design and implement a model to solve the problem: 
+1. Estimating rainwater harvesting capacity: a. Rainfall estimation: Historical data from rainfall gauges at different places in the target area. b. Catchment area: Masterplan of the city to estimate the catchment area available, e.g open areas like rooftop, courtyard, etc.  
+2. Optimizing Water tank placement:  
+   - a. Water demand/Use capacity: Water supply data can be used to estimate the consumption of harvested rainwater for non-drinking purposes 
+   - b. Underground map: Underground map with stability study to identify locations where the shared tank can be built The system should provide the following output from its analysis: 
+      - 1. Plan for laying out the underground tanks with input and output points defined 
+      - 2. Cost benefit analysis justifying the plan 
+      - 3. Plan for distribution of build and maintenance cost of a tank for the parties involved
+ ---
+ ### Solutions
+- #### Rooftop Detection can be carried out with three main approaches which are:
+1. `YOLO Algorithm and Object Detection using Tensorflow`: This might result in problems of incorrect labelling of the rooftops and can lead to inaccuracy which becomes a problem to a certain degree posing a threat to deployability if seen on a massive scale.
+2. `Open Computer Vision` is a graphical image processing library known for better processing of lower to higher resolution of images. Considering the case of India, getting high resolution satellite images for different inert areas is extremely difficult and thus needs a lot of preprocessing which is much easier and faster to carry out in OpenCV.
+3. `Convolution Neural Networks` require a lot of preprocessing in case of satellite images and can become a problem if the availability of large amounts of data is not present. Though the performance increases with every forward propogation, the current architectural constraints in deploying Kubernetes clusters makes OpenCV the most viable option.
+Thus, we have opted for the OpenCV approach to accurately detect, estimate and measure the roof dimensions.
+![ALT-IMG](https://github.com/Vishal-V/SIH-RainWater-Harvesting/blob/master/fig1.png)  
+  
+- #### 2. Depth Estimation using Digital Elevation Model (DEM)
+  - The tank dimensions are calculated from the rooftop catchment estimates obtained from contour mapping of the roof. The bounding boxes drawn on the roof takes the scale of the images into account and calculates the catchment area from the roof detected output
+  - The contour mapping draws bounding boxes on the roof and outputs the catchment area in the location matrix limit as provided by the user's image.
+  - The `Depth Estimation` is computed from the `Digital Elevation Modelling algorithm` that calculates the least elevation point by partitioning the image into boxes and then calculates the local minima of every box. The resulting global minima is considered as the least elevation point for the given area matrix.  
+    
+![ALT-IMG](https://github.com/Vishal-V/SIH-RainWater-Harvesting/blob/master/static/images/dem.png)
+ 
+- #### 3. Rainfall Prediction
+  - The rainfall prediction has been trained on 100 years' data for the city of Bangalore. We have used a time series plot uisng ARIMA models as well as a statistical analysis to obtain the values for various months.
+![ALT](https://github.com/Vishal-V/SIH-RainWater-Harvesting/blob/master/static/images/dash.png)  
+- #### 4. Calculator (Cost of installation + Break-Even analysis)
+  - This is the complete analysis pipeline for the project. It initially starts with the user uploading a satellite image from our Serach bar and the backend Computer Vision models(`OpenCV`) and machine learning models(`K-Means clustering`) calculate the catchment are of the rooftops under consideration.
+  - The calculated area is then fed to our moldes to estimate the cost of installation of the entire setup and also finds the maintenance cost as well.
+![ALT](https://github.com/Vishal-V/SIH-RainWater-Harvesting/blob/master/static/images/calc.png)  
+- #### 5. Non-Drinking Water Usage Research
+    - This was a research oriented part of the project to estimate and validate the non-drinking water usage of a certain area that also takes the water usage and lifestyle into account.
+    - The papers based on water usage was maonly for domestic purposes and very few for industrial usage. Hence, our end-to-end pipeline is a B2C model rather than a B2B model
+  
+- #### 6. Basic Shared Rainwater Harvesting System (Phase 1)
+  - This is a fully working model of the Shared Rainwater Harvesting system that has an easy-to-use UI with accurate predictions using AI and Computer Vision models to predict the most optimal spot to detect the location to install the tank and also provides complete financial analysis and break-even time for the installation. Here, you can find the picture of the installation on our college hostel.
+![ALT](https://github.com/Vishal-V/SIH-RainWater-Harvesting/blob/master/static/images/point.png)
+#### 7. Underground Map
+  
+  The problem of designing the underground map involves many parameters including obstacles beneath the ground a layout of the pipeline from inlet to outlet of the rainwater harvesting solution.  
+  This essentially means that there is a requirement of searching algorithm to traverse a path which can be `optimal` and also does not traverse the path with obstacles. The obstacles can range from gas pipelines beneath the ground with a vulnerable range and sewage system under the ground as well as subway lines constructed. In this case scenario, the underground map needs to traverse a path which does not have any obstacle in the way and remains optimal atmost of the times thereby giving an effective and deployable solution.
+    
+The underground map consists of two phase possible implementation:
+ 1. Heat Map: The heat map of the region detects varied different objects under the ground and provides a label to each of them in form of a different colour. When provided with the data of locations of the obstacles such as gas pipelines it can be used to label the specific colour and then provide an optimal path to the tank such that it does not get into the path of the gas pipelines.
+ 2. Searching Technique: The final half of the underground map is to provide the optimal path which is cost effective and does not hinder the existing obstacles that is gas pipelines and sewage pipes underground. This is an effective and novel approach in simple fashion and can be a generic model that can be easily adapted by any rainwater harvesting solution. Best First Search is an optimal and useful technique implemented as a possible solution for this problem.
+ ![ALT](https://github.com/Vishal-V/SIH-RainWater-Harvesting/blob/master/undeground_map.gif)
+
---
+++ app.py
@@ -0,0 +1,269 @@
+from __future__ import print_function
+from skimage.feature import peak_local_max
+from skimage.morphology import watershed
+from scipy import ndimage
+import numpy as np
+import matplotlib.pyplot as plt
+from flask import Flask, render_template, url_for, redirect, request, flash, request
+from scipy.spatial import distance as dist
+from imutils import perspective
+from imutils import contours
+import argparse
+import imutils
+import json
+import urllib
+import cv2
+import os
+from werkzeug.utils import secure_filename
+from PIL import Image
+
+UPLOAD_FOLDER = './static/images'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask("__app__")
+app.config['SECRET_KEY'] = 'a551d32359baf371b9095f28d45347c8b8621830'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload_file', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return render_template('calculator.html', val=1)
+
+from flask import send_from_directory
+
+# Watershed Algorithm
+def equalize(img):
+    ycrcb = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+    channels = cv2.split(ycrcb)
+    cv2.equalizeHist(channels[0], channels[0])
+    cv2.merge(channels, ycrcb)
+    cv2.cvtColor(ycrcb, cv2.COLOR_YCR_CB2BGR, img)
+    return img
+
+def Watershed(location_det):
+	print(location_det)
+	image = cv2.imread(location_det)
+	im = equalize(image)
+	shifted = cv2.pyrMeanShiftFiltering(image, 21, 51)
+
+	gray = cv2.cvtColor(shifted, cv2.COLOR_BGR2GRAY)
+	thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+	plt.imshow(thresh, cmap=plt.get_cmap('gray'))
+
+	D = ndimage.distance_transform_edt(thresh)
+	localMax = peak_local_max(D, indices=False, min_distance=20,
+	                          labels=thresh)
+
+	markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
+	labels = watershed(-D, markers, mask=thresh)
+	print("[INFO] {} unique segments found".format(len(np.unique(labels)) - 1))
+
+	for label in np.unique(labels):
+	    if label == 0:
+	        continue
+	    mask = np.zeros(gray.shape, dtype="uint8")
+	    mask[labels == label] = 255
+	    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+	    
+	    for (i, c) in enumerate(cnts):
+	 
+	        ((x, y), _) = cv2.minEnclosingCircle(c)
+	        cv2.putText(image, "#{}".format(i + 1), (int(x) - 10, int(y)),
+	                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+	        cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+
+	# plt.savefig('./static/images/fig1.png')
+	plt.figure()
+	plt.imshow(image, cmap='gray')
+	plt.axis('off')
+	plt.savefig('fig1.png')
+	imag = Image.open('fig1.png')
+	imag.show()
+	
+# To measure the dimensions
+def midpoint(ptA,ptB):
+    return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+
+def measure_dim(loc):
+
+	# ap = argparse.ArgumentParser()
+	# ap.add_argument("-i", "--image", required = True, help="path to input image")
+	# ap.add_argument("-w", "--width", type=float, required=True, help="width of the object")
+	# args = vars(ap.parse_args())
+	image = cv2.imread(loc)
+	# image = cv2.imread(args["image"])
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (7,7), 0)
+
+	edged = cv2.Canny(gray, 50, 100)
+	edged = cv2.dilate(edged, None, iterations = 1)
+	edged = cv2.erode(edged, None, iterations = 1)
+
+	cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+	(cnts, _) = contours.sort_contours(cnts)
+	pixelsPerMetric = None
+
+	for c in cnts:
+	    if cv2.contourArea(c) < 100:
+	        continue
+	    orig = image.copy()
+	    box = cv2.minAreaRect(c)
+	    box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+	    box = np.array(box, dtype="int")
+	    box = perspective.order_points(box)
+	    cv2.drawContours(orig, [box.astype("int")], -1, (0,255,255), 2)
+	
+	for (x,y) in box: 
+	    cv2.circle(orig,(int(x),int(y)), 5, (0,0,255), -1)
+	
+	(tl, tr, br, bl) = box
+	(tltrX, tltrY) = midpoint(tl,tr)
+	(blbrX, blbrY) = midpoint(bl,br)
+	(tlblX,tlblY) = midpoint(tl,bl)
+	(trbrX, trbrY) = midpoint(tr,br)
+
+	#Draw the midpoints on the image
+	cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+	cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+	cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+	cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+
+	#intersect the lines between midpoints
+	cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+			(255,255, 255), 2)
+	cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+			(255, 255, 255), 2)
+	#compute the Euclidean distance between midpoints
+	dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+	dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+	#We initialize the pixels per metric has not been established 
+	if pixelsPerMetric is None:
+	    pixelsPerMetric = dB / 750
+	dimA = dA / pixelsPerMetric
+	dimB = dB / pixelsPerMetric
+	#to compute the final object size
+	cv2.putText(orig, "{:.1f} feet".format(dimA*10),
+			(int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
+			0.65, (255, 0, 0), 2)
+	cv2.putText(orig, "{:.1f} feet".format(dimB),
+			(int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
+			0.65, (255, 0, 0), 2)
+	area = dimA*dimB
+	dims = area
+	print(f'The dims: {dims}')
+	return dims
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+	# print('./static/images/'+filename)
+	Watershed(str('./static/images/'+filename))
+	dims = measure_dim(str('./static/images/'+filename))
+	dims/=10000
+	return render_template('calculator.html', val=1, dims=dims)
+
+# Least Geographic Elevation
+def elevation(request):
+	apikey = "AIzaSyDv9C5WnFwlmPtZWMtH6EqfMhSwJrlCcD0"
+	url = "https://maps.googleapis.com/maps/api/elevation/json"
+	request = urllib.request.urlopen(url+"?locations="+str(request)+"&key="+apikey)
+	try:
+		results = json.load(request).get('results')
+		if 0 < len(results):
+			mat={}
+			for i in range(0,len(results)):	    
+				elevation = results[i].get('elevation')
+				location=results[i].get('location')
+				loclat=[]
+				loclat.append(location['lat'])
+				loclat.append(location['lng'])
+				loc=tuple(loclat)
+				if elevation not in mat:
+					mat[elevation]=[]
+				mat[elevation].append(loc)
+				# ELEVATION
+			return mat
+		else:
+			print ('HTTP GET Request failed.')
+	except ValueError as e:
+		print ('JSON decode failed: '+str(request))
+
+def postion(lat1,lon1,lat2,lon2):
+  if(lat1>lat2):  # swaping cordinates to get range of latitude and longitude 
+    temp=lat1
+    lat1=lat2
+    lat2=temp
+  if(lon1>lon2):
+    temp=lon1
+    lon1=lon2
+    lon2=temp
+  res=''   #initializing string with null value
+  i=0.0
+  i=lat1	#itration variable for varying latitude
+  while i<lat2:
+    j=0.0
+    j=lon1	#iteration variable for varying longitude
+    while j<lon2:
+      res=res+(str(i))	# adding current latitude to string
+      res=res+','	# separator for latitude and longitude
+      if((i+0.0001)>=lat2 and (j+0.0001)>=lon2):	#cheacking wheather the coordinate is last one
+        res=res+(str(j))	# last coordinate need not to have '|' as its the last one
+      else:
+        res=res+(str(j))	#else we need to saperate the coordinates with '|'
+        res=res+'|'	#adding '|' after one coordinate is entered
+      j=j+0.0001		#increasing longitude by 10 meters
+    i=i+0.0001		#increasing latitude by 10 meters
+  result=elevation(res)		#calling elevaton function to get elevation data
+  rest={}			#dictonary 
+  for key in sorted(result.keys()):	#getting elevation in sorted order
+    rest[key]=result[key]	
+  return rest		#getting elevated data in increasing order
+
+# All routes beyond this
+@app.route('/')
+def home():
+	return render_template('index.html', title='SIH 2019')
+
+@app.route('/rooftop')
+def rooftop():
+	return render_template('roof.html', title='Rooftop Detection')
+
+@app.route('/references')
+def references():
+	return render_template('references.html', title='References')
+
+@app.route('/calculator', methods=['GET','POST'])
+def calculator():
+	pos = postion(13.00011,77.00011,13.0011,77.00111)
+	return render_template('calculator.html', title='Calculator', position=pos, val=1)
+
+@app.route('/trial', methods=['GET'])
+def trial():
+	pos = postion(13.00011,77.00011,13.0011,77.00111)
+	return render_template('trial.html', position=pos)
+
+@app.route('/dashboard')
+def dashboard():
+	return render_template('dashboard.html')
+
+@app.route('/contour')
+def contour():
+	return render_template('contour_map.html', title='Contour Map')
+
+app.run(debug=True, port=5003)
